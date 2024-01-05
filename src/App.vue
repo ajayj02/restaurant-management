358a5f3216d749a9aa005350ed2c06fd39efb122
Ajay Joshi <joshiajay311@gmail.com>
2024-01-06 00:27:42 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import Mycomp from './components/Mycomp.vue'
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="Bash on regarless" />
+      <Mycomp name="Deepak" gender="his" callsign="Dick"/>
+    </div>
+  </header>
+</template>
+
---
+++ src/components/Mycomp.vue
@@ -0,0 +1,27 @@
+<template>
+    <h1>This is {{ name }}'s component and {{gender}} callsign is {{ callsign }}</h1>
+    <h2>This is my email address: {{ email }}</h2>
+    <h2>This is my mobile number: {{ mobile }}</h2>
+    <h1>Developer's Full Name: {{ getname('Ajay Joshi') }} address: {{ getAddress() }}</h1>
+    </template>
+
+<script>
+export default {
+props: ['name','callsign','gender'],
+name: "Mycomp",
+data() {
+    return {
+        email: 'joshiajay311@gmail.com',
+        mobile: '+91-8586934166',
+        getname: function(arg) {
+            return arg
+        }
+    }
+},
+methods:{
+    getAddress() {
+        return "gole market New delhi-110001"
+    }
+}
+}
+</script>
